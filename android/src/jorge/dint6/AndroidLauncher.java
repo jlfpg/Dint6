ab2a7f06a9222dd6d0060bdd14f53e4b7a00470d
Jorge López <jorge.lopez@live.u-tad.com>
2018-02-13 17:55:01 +0100

    root commit

---
+++ android/src/jorge/dint6/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package jorge.dint6;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import jorge.dint6.GameMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameMain(), config);
+	}
+}
